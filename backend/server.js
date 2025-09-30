64e120d3ae24e2604da525cbfc869dc7018e446c
Your Name <Panyawatfaktim1209@gmail.com>
2025-10-01 01:23:11 +0700

    first commit

--- backend/server.js
+++ backend/server.js
@@ -7,8 +7,10 @@ const fs = require("fs");
 const path = require("path");
 
 const app = express();
-const PORT = 3000;
-
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, '0.0.0.0', () => {
+  console.log(`Server running on port ${PORT}`);
+});
 // Middleware
 app.use(cors());            
 app.use(express.json());
